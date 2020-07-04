cf7488a6e6edf7fee9d16059e47fe390c3440d96
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-07-03 22:26:24 -0400

    Updates

--- src/actions/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const FETCH_RESTAURANTS = 'FETCH_RESTAURANTS';
-export const GET_CITY = 'GET_CITY';
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { rootReducer } from './restaurantReducer';
-
-export default combineReducers({
-  restaurants: rootReducer
-});

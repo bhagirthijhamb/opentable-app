75e8e7a0430904fac492e0345f5ed29ddf52b592
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-07-03 22:50:42 -0400

    Updates

---
+++ precache-manifest.d4f323e3e6729ec67008adcf6b8dc315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007b19bfd2b1bb839189c7760c34e062",
+    "url": "/bhagirthijhamb/opentable-app/index.html"
+  },
+  {
+    "revision": "fd6e0bdb9311cf70fe1f",
+    "url": "/bhagirthijhamb/opentable-app/static/css/main.a150f9b9.chunk.css"
+  },
+  {
+    "revision": "e707a1ddd8dc35d42631",
+    "url": "/bhagirthijhamb/opentable-app/static/js/2.b740c5fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bhagirthijhamb/opentable-app/static/js/2.b740c5fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd6e0bdb9311cf70fe1f",
+    "url": "/bhagirthijhamb/opentable-app/static/js/main.6ffabbbc.chunk.js"
+  },
+  {
+    "revision": "ff7c82afdaef62ec3543",
+    "url": "/bhagirthijhamb/opentable-app/static/js/runtime-main.88e11c78.js"
+  }
+]);
